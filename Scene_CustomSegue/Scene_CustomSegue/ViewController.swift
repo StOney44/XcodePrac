7df027c28473fc602a702ea789ebc9f1b6966f50
StOney_Ena <kmhgh44@gmail.com>
2022-09-29 23:21:37 +0900

    segue

--- Scene_CustomSegue/Scene_CustomSegue/ViewController.swift
+++ Scene_CustomSegue/Scene_CustomSegue/ViewController.swift
@@ -13,7 +13,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+                
+        if(segue.identifier == "custom.segue") {
+            NSLog("커스텀 세그가 실행됩니다")
+        }
+        else if(segue.identifier == "action.segue") {
+            NSLog("액션 세그가 실행됩니다")
+        }
+        else {
+            NSLog("알 수 없는 세그입니다.")
+        }
+    }
 
 }
+
 
